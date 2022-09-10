c236843c0a0a51403d9737cbb6cdd1c652c16b64
Md Talib <95065465+talib789@users.noreply.github.com>
2022-09-10 19:40:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import ManufacturersList from "./components/ManufacturersList";
+import OperatingSystemList from "./components/OperatingSystemList";
+import "./styles.css";
+
+export default function App() {
+  const OSList = ["ANdroid", "Blackberry", "iphone", "Windows Phone"];
+  const Mfgs = ["Samsung", "HTC", "Micromax", "Apple"];
+  return (
+    <div className="App">
+      <h1>Mobile Operating System</h1>
+
+      <ul>
+        <OperatingSystemList OSList={OSList} />
+      </ul>
+      <h1>Mobile Manufacturers</h1>
+      <ul>
+        <ManufacturersList Mfgs={Mfgs} />
+      </ul>
+    </div>
+  );
+}
